af4f988a5688d2ea7e7df14279d5384d8471f71d
YU-K <xver456@gmail.com>
2023-07-27 12:08:53 +0400

    Add game engine

---
+++ brain_games/engine.py
@@ -0,0 +1,21 @@
+#!/usr/bin/venv python3
+import prompt
+
+
+def game_engine(get_game_data, task):
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(task)
+    rounds_count = 3
+    while rounds_count > 0:
+        right_answer, question = get_game_data()
+        print(f"Question: {question}")
+        user_answer = int(prompt.string("Your answer: "))
+        if user_answer == right_answer:
+            print("Correct!")
+            rounds_count -= 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'. Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
